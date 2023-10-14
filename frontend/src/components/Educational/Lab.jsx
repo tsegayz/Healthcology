de76843b8ee2440e4026b2e7d48aae685741f6e8
Tsega Yaekob <tsegayaekob7@gmail.com>
2023-10-14 14:36:18 +0300

    labs page

---
+++ frontend/src/components/Educational/Lab.jsx
@@ -0,0 +1,15 @@
+import res1 from "./assets/home/research1.jpeg";
+
+function Lab() {
+	return (
+		<div className='labs'>
+			<div className='logo'>
+				<h1>
+					H<span>ealthCology</span>
+				</h1>
+			</div>
+		</div>
+	);
+}
+
+export default Lab;
